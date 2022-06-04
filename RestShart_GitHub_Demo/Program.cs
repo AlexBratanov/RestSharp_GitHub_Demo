f78be16ef6d3e0e0e0d9746f6ba7abc1b7c610db
Alex <Alex@DESKTOP-T1ABSJL>
2022-06-04 17:54:46 +0300

    Add project files.

---
+++ RestShart_GitHub_Demo/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+using RestSharp;
+using System;
+
+var client = new RestClient("https://api.githug.com");
+
+var request = new RestRequest("/repos/{user}/{repo}/issues/{id}");
+
+
+request.AddUrlSegment("user", "AlexBratanov");
+request.AddUrlSegment("repo", "Postman");
+request.AddUrlSegment("id", 4);
+
+
+var response = await client.ExecuteAsync(request);
+
+Console.WriteLine("STATUS CODE " + response.StatusCode);
+Console.WriteLine("BODY: " + response.Content);
